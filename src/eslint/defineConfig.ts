ed71574a1f1fb6b7a6e16c5e820aff3901de721e
jfreyheit <jfreyheit@gmail.com>
2025-01-06 23:59:40 +0700

    fix: fix typescript resolver (2nd try) (#34)

--- src/eslint/defineConfig.ts
+++ src/eslint/defineConfig.ts
@@ -70,7 +70,7 @@ export function defineConfig({
     },
     {
       settings: {
-        "import/resolver-next": {
+        "import/resolver": {
           typescript: {
             alwaysTryTypes: true, // always try to resolve types under `<root>@types` directory even it doesn't contain any source code, like `@types/unist`
             extensions: [
